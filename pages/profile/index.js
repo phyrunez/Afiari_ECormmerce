d292bc85e22501806f205bdd6739523820530933
bankole <bankoleibrahim96@gmail.com>
2022-05-13 21:43:09 +0100

    made changes to the pages

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,12 +1,13 @@
 import { Box } from '@mui/material';
 import React from 'react';
-import ProfileSideNav from '../components/Profile/ProfileSideNav';
-import ShoppingHistory from '../components/Profile/ShoppingHistory';
-import Navbar from '../src/shared-components/navbar/Navbar';
-import Footer from '../src/page-components/Footer';
+import ProfileSideNav from '../../components/Profile/ProfileSideNav';
+import ShoppingHistory from '../../components/Profile/ShoppingHistory';
+import Navbar from '../../src/shared-components/navbar/Navbar';
+import Footer from '../../src/page-components/Footer';
 import { useSelector } from 'react-redux';
 
 function Profile() {
+  const mo = true;
   return (
     <Box
       sx={{
@@ -28,9 +29,7 @@ function Profile() {
         }}
       >
         <ProfileSideNav />
-        <Box>
-          <ShoppingHistory />
-        </Box>
+        <Box>{mo ? 'h1' : <ShoppingHistory />}</Box>
       </Box>
       <Footer />
     </Box>
