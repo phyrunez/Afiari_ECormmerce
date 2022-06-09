372e657a1d0197f5f09acd9b6ec0be751c2d617d
Chiexclusive <chiexclusive1151@gmail.com>
2022-06-09 01:14:50 +0100

    made some changes

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -10,118 +10,118 @@ import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 import BackButton from '../src/shared-components/BackButton';
 
-const ResetPassword = () => {
+const ResetPassword = ({ isLinkVerified, err_message }) => {
   const router = useRouter();
-
   const dispatch = useDispatch();
+  const email = router.query.email;
+  const token = router.query.token;
 
-  const {
-    reset_password,
-    country,
-    token,
-    email,
-    password,
-    loading,
-    repeat_password,
-  } = useSelector((state) => state.auth);
+  const { password, loading, repeat_password } = useSelector(
+    (state) => state.auth
+  );
 
   const onSubmit = (e) => {
     e.preventDefault();
+
     if (email === '' || password === '') {
       toast.error('Fields can not be empty');
     } else if (password !== repeat_password) {
       toast.error('Password does not match');
     } else {
       const userData = {
-        username: email,
+        email,
         token: token,
         newPassword: password,
         confirmNewPassword: repeat_password,
       };
 
-      dispatch(resetPassword(userData));
-
-      toast.success('Your password has been reset successfully.');
-      router.push('/password-reset-success');
+      dispatch(resetPassword(userData)).then((args) => {
+        console.log(args);
+        toast.success('Your password has been reset successfully.');
+        router.push('/password-reset-success');
+      });
     }
   };
 
-  return (
-    <Box
-      sx={{
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '4rem',
-      }}
-    >
-      <BackButton />
-      <AuthenticationPages
-        heading="RESET PASSWORD"
-        subHeading="Set a new password for your profile on Afiari"
-        paddinglg="10px"
-        paddingxs="10px"
-      />
-      <Input
-        type="text"
-        label="Email"
-        htmlFor="email"
-        placeholder="somebody@mail.com"
-        onChange={(e) => {
-          dispatch(handleUserInput('email', e.target.value));
+  if (isLinkVerified) {
+    return (
+      <Box
+        sx={{
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '4rem',
         }}
-        name="email"
-        id="email"
-        value={email}
-      />
-      <Input
-        type="password"
-        label="password"
-        htmlFor="password"
-        placeholder="************"
-        onChange={(e) => {
-          dispatch(handleUserInput('password', e.target.value));
-        }}
-        name="password"
-        id="password"
-        value={password}
-      />
-      <Input
-        type="password"
-        label="password"
-        htmlFor="repeat_password"
-        placeholder="************"
-        onChange={(e) => {
-          dispatch(handleUserInput('repeat_password', e.target.value));
-        }}
-        name="repeat_password"
-        id="repeat_password"
-        value={repeat_password}
-      />
-      <Input
-        type="text"
-        label="token"
-        htmlFor="token"
-        placeholder="************"
-        onChange={(e) => {
-          dispatch(handleUserInput('token', e.target.value));
-        }}
-        name="token"
-        id="token"
-        value={token}
-      />
-
-      <Button
-        text="RESET PASSWORD"
-        color="#fff"
-        backgroundColor="#0A503D"
-        onClick={onSubmit}
-        isLoading={loading}
-      />
-    </Box>
-  );
+      >
+        <BackButton />
+        <AuthenticationPages
+          heading="RESET PASSWORD"
+          subHeading="Set a new password for your profile on Afiari"
+          paddinglg="10px"
+          paddingxs="10px"
+        />
+        <Input
+          type="password"
+          label="password"
+          htmlFor="password"
+          placeholder="************"
+          onChange={(e) => {
+            dispatch(handleUserInput('password', e.target.value));
+          }}
+          name="password"
+          id="password"
+          value={password}
+        />
+        <Input
+          type="password"
+          label="password"
+          htmlFor="repeat_password"
+          placeholder="************"
+          onChange={(e) => {
+            dispatch(handleUserInput('repeat_password', e.target.value));
+          }}
+          name="repeat_password"
+          id="repeat_password"
+          value={repeat_password}
+        />
+        <Button
+          text="RESET PASSWORD"
+          color="#fff"
+          backgroundColor="#0A503D"
+          onClick={onSubmit}
+          isLoading={loading}
+        />
+      </Box>
+    );
+  } else {
+    return <Box>{err_message}</Box>
+  }
 };
 
+export async function getServerSideProps(context) {
+  const email = context.query.email;
+  const token = context.query.token;
+  if (!email || !token) return {
+    props: { isLinkVerified: true, err_message: 'Invalid email' },
+  }
+  try {
+    const result = await verifyPasswordReset(email, token)
+    if (result.status === true) {
+      return {
+        props: { isLinkVerified: true, err_message: 'Invalid email' }
+      }
+    } else {
+      return {
+        props: { isLinkVerified: false, err_message: 'Invalid email' },
+      };
+    }
+  } catch (err) {
+    return {
+      props: { isLinkVerified: false, err_message: 'Server Error! Reload page to continue.' },
+    };
+  }
+}
 export default ResetPassword;
+
