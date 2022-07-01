bba3117719f41670e59b2c95654d5e7d650b5503
GEORGE EBISIKE <georgeebisike@gmail.com>
2022-07-01 12:34:23 +0100

    added google analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ import ProtectedRoutes from '../constants/ProtectedRoutes';
 import { CartProvider } from 'react-use-cart';
 import { useEffect } from 'react';
 import { getAllCountries } from '../src/redux/general/generalAction';
+import Script from 'next/script'
 
 const store = configureStore();
 // const persistor = persistStore(store);
@@ -44,19 +45,7 @@ Where can I buy foodstuff near me,
 Foodstuff shop,
 Foodstuff market in Lagos"
         />
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-YDBXXY5PPK"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'G-YDBXXY5PPK');
-      </script> */}
-        {/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> */}
-        {/* <script src="../src/vendor/jquery.min.js"></script>
-        <script src="../src/vendor/sweetalert.min.js"></script> */}
       </Head>
 
       <Provider store={store}>
@@ -70,6 +59,43 @@ Foodstuff market in Lagos"
         </CartProvider>
         {/* </PersistGate> */}
       </Provider>
+
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-YDBXXY5PPK" />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YDBXXY5PPK');
+          `
+        }
+      </Script>
+
+
+      {/* <!-- Meta Pixel Code --> */}
+      {/* <Script strategy="afterInteractive">
+        !function(f,b,e,v,n,t,s)
+        {
+            if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+              n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)
+    }
+    (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '393131539190809');
+            fbq('track', 'PageView');
+      </Script>
+      <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=393131539190809&ev=PageView&noscript=1"
+      /></noscript> */}
+      {/* <!-- End Meta Pixel Code --> */}
 
       <ToastContainer />
     </>
